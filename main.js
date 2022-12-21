0226c2ab2aaa0370a3dc41874c0d966af2537e07
Mr.Gamer <nicklaus.vega21@gmail.com>
2022-12-21 10:48:09 -0600

    starter

--- main.js
+++ main.js
@@ -2,15 +2,15 @@ document.onreadystatechange = function () {
   if (document.readyState !== "complete") {
     console.log("loading");
     document.querySelector("#loader").style.display = "visible";
-    setTimeout(function () {
-    document.querySelector("body").style.visibility = "hidden";
-    }, 3000);
+
+    document.querySelector("#main").style.visibility = "hidden";
+
 
   } else {
     console.log("loaded");
     setTimeout(function () {
 
-    document.querySelector("body").style.visibility = "visible";
+    document.querySelector("#main").style.visibility = "visible";
 
     document.querySelector("#loader").style.display = "none";
     }, 3000);
